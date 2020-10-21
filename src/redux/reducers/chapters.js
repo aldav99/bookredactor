c2b0c364aae08f70ffc2cf4c626b6f0a421eaaba
aldav99 <aldav99@yandex.ru>
2020-10-21 07:34:17 +0300

    Try2

--- src/redux/reducers/chapters.js
+++ src/redux/reducers/chapters.js
@@ -12,7 +12,52 @@ export const chapters = function (state = initialState, action) {
                 )
             )
         case 'ADD_CHAPTER':
-            return state.concat({ id: state.length + 1, text: action.text, completed: false, numberOfSections: 0, numberOfCompletedSections: 0  })
+            return state.concat({ id: state.length + 1, text: action.text, completed: false, numberOfSections: 0, numberOfCompletedSections: 0 })
+        case 'ADD_SECTION':
+            return state.map(
+                (chapter, idx) => (
+                    chapter === action.chapter
+                        ? { ...chapter, numberOfSections: chapter.numberOfSections + 1 }
+                        : chapter
+                )
+            )
+        case 'TOGGLE_SECTION':
+            if (action.section.completed && action.chapter.completed) {
+                return state.map(
+                    (chapter, idx) => (
+                        chapter === action.chapter
+                            ? { ...chapter, completed: false, numberOfCompletedSections: chapter.numberOfCompletedSections - 1 }
+                            : chapter
+                    )
+                )
+            }
+            if (action.section.completed && !(action.chapter.comleted)) {
+                return state.map(
+                    (chapter, idx) => (
+                        chapter === action.chapter
+                            ? { ...chapter, numberOfCompletedSections: chapter.numberOfCompletedSections - 1 }
+                            : chapter
+                    )
+                )
+            }
+            if (!(action.section.completed) && (action.chapter.numberOfSections === action.chapter.numberOfCompletedSections + 1)) {
+                return state.map(
+                    (chapter, idx) => (
+                        chapter === action.chapter
+                            ? { ...chapter, completed: true, numberOfCompletedSections: chapter.numberOfCompletedSections + 1 }
+                            : chapter
+                    )
+                )
+            }
+            if (!(action.section.completed) && !(action.chapter.numberOfSections === action.chapter.numberOfCompletedSections + 1)) {
+                return state.map(
+                    (chapter, idx) => (
+                        chapter === action.chapter
+                            ? { ...chapter, numberOfCompletedSections: chapter.numberOfCompletedSections + 1 }
+                            : chapter
+                    )
+                )
+            }
         default:
             return state
     }
--- src/redux/reducers/sections.js
+++ src/redux/reducers/sections.js
@@ -4,20 +4,8 @@ const initialState = []
 export const sections = function (state = initialState, action) {
     switch (action.type) {
         case 'ADD_SECTION':
-            action.chapter.numberOfSections = action.chapter.numberOfSections + 1
             return state.concat({ id: action.chapter.id, text: action.text, completed: false })
         case 'TOGGLE_SECTION':
-            if (action.section.completed) {
-                action.chapter.numberOfCompletedSections = action.chapter.numberOfCompletedSections - 1
-            } else {
-                action.chapter.numberOfCompletedSections = action.chapter.numberOfCompletedSections + 1
-            }
-
-            if (action.chapter.numberOfSections && action.chapter.numberOfCompletedSections === action.chapter.numberOfSections) {
-                action.chapter.completed = true
-            } else {
-                action.chapter.completed = false
-            }
             return state.map(
                 (section, idx) => (
                     (section === action.section)
