b45afe526a3b0d3ac7d36e97241dff8ecab25131
aldav99 <aldav99@yandex.ru>
2020-10-30 07:58:13 +0300

    Add thunk

--- src/redux/actions/chapters.js
+++ src/redux/actions/chapters.js
@@ -1,4 +1,5 @@
 import * as chaptersActions from '../actionType/chapters'
+import axios from 'axios'
 
 import { API_CALL } from '../middleware/API'
 
@@ -24,14 +25,36 @@ export const toggleSection = (section, chapter) => ({
     chapter
 })
 
-export const fetchChapters = () => ({
-    [API_CALL]: {
-        endpoint: '/chapters',
-        method: 'GET',
-        types: [
-            chaptersActions.FETCH_CHAPTER_REQUEST,
-            chaptersActions.FETCH_CHAPTER_SUCCESS,
-            chaptersActions.FETCH_CHAPTER_FAILURE
-        ]
+// export const fetchChapters = () => ({
+//     [API_CALL]: {
+//         endpoint: '/chapters',
+//         method: 'GET',
+//         types: [
+//             chaptersActions.FETCH_CHAPTER_REQUEST,
+//             chaptersActions.FETCH_CHAPTER_SUCCESS,
+//             chaptersActions.FETCH_CHAPTER_FAILURE
+//         ]
+//     }
+// })
+
+export const fetchChapters = () => (
+    (dispatch) => {
+        dispatch({
+            type: chaptersActions.FETCH_CHAPTER_REQUEST
+        })
+
+        return axios({
+            method: "GET",
+            url: 'https://chapters-74b6.restdb.io/rest/chapters',
+            headers: {
+                'x-apikey': '5f98ec2b231ba42851b49e54'
+            }
+        }).then(res => dispatch({
+            type: chaptersActions.FETCH_CHAPTER_SUCCESS,
+            response: res.data
+        })).catch(error => dispatch({
+            type: chaptersActions.FETCH_CHAPTER_FAILURE,
+            error
+        }))
     }
-})
+)
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,11 +4,13 @@ import rootReducer from './reducers'
 
 import APIMiddleware from './middleware/API'
 
+import thunkMiddleware from 'redux-thunk'
+
 
 const store = createStore(
     rootReducer,
     compose(
-        applyMiddleware(APIMiddleware),
+        applyMiddleware(thunkMiddleware),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 )
