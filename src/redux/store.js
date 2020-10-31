65b211a9c981c598fde0da87caf14e9d173a33f9
aldav99 <aldav99@yandex.ru>
2020-10-31 08:27:58 +0300

    Add Configurestor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,15 @@
-import { createStore, compose, applyMiddleware } from 'redux'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 
 import rootReducer from './reducers'
 
 import thunkMiddleware from 'redux-thunk'
 
 
-const store = createStore(
-    rootReducer,
-    compose(
-        applyMiddleware(thunkMiddleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-)
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware(thunkMiddleware),
+    devTools: process.env.NODE_ENV !== 'production'
+})
 
 
 export default store;
